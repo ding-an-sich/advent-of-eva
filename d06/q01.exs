311f9aa26c80115034f947d574d357dbce6e230b
Vinicius M.R. de Carvalho <vmrc@ding-an-sich.com>
2022-12-06 07:41:03 -0300

    day 06

---
+++ d06/q01.exs
@@ -0,0 +1,17 @@
+File.read!("./input")
+|> String.trim()
+|> String.graphemes()
+|> Enum.with_index()
+|> Enum.chunk_every(4, 1)
+|> Enum.reduce_while([], fn chunk, acc ->
+  if chunk
+     |> Enum.uniq_by(fn {a, _} -> a end)
+     |> length() ==
+       length(chunk) do
+    {_, i} = chunk |> List.last()
+    {:halt, i + 1}
+  else
+    {:cont, []}
+  end
+end)
+|> dbg()
